2581edd256014cca1101bf4c417ed21ee35ab7fb
Florian Hölzel <florianhoelzel@icloud.com>
2026-08-09 22:45:47 +0200

    Promote v2 landing page to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import LandingPage from "./landing-page";
+import { demoRunnerData } from "./data/demo-runner";
+import BackToSearchButton from "./v2/back-to-search-button";
+import DemoArchivePreview from "./v2/demo-archive-preview";
+import V2LandingPage from "./v2/v2-landing-page";
+import "./v2/v2.css";
 
 export default function Home() {
-  return <LandingPage />;
+  return (
+    <>
+      <V2LandingPage />
+      <DemoArchivePreview data={demoRunnerData} />
+      <BackToSearchButton />
+    </>
+  );
 }
